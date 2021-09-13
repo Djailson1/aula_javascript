7b22f2f61697ad40d3a8ca1e73e241d3fe14c50c
Djailson1 <89245910+Djailson1@users.noreply.github.com>
2021-09-12 23:50:50 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,111 @@
+
+function clicou(){
+document.getElementById("Agradecimento").innerHTML = "<b>Obrigado por clicar</b>"; }
+//console.log(document.getElementById("Agradecimento"));
+    
+    /*
+{
+    alert("Obrigado por clicar");
+}*/
+
+function redirecionar(){
+window.open("https://www.facebook.com/");
+    //window.location.href = "https://www.facebook.com/";
+}
+
+function trocar(elemento){
+   elemento.innerHTML = "Obrigado por passar o mouse";
+    //alert("Trocar texto");
+}
+
+function voltar(elemento){
+    elemento.innerHTML = "passe o mouse aqui";
+}
+function load(){
+    alert("Página carregada")
+}
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}
+
+/*function soma (n1,n2){
+    return n1 + n2
+} 
+var validar = 0;
+function validaIdade(idade){
+    if(idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    
+
+}
+var idade = prompt ("Qual a sua idade");
+validaIdade(idade)
+console.log(validar);*/
+
+
+/*var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());*/
+
+/*var count = 0;
+for(count = 0; count <= 5; count++) {
+    alert(count);
+};*/
+
+
+
+
+/*var count = 0;
+while ( count <= 5){
+    console.log(count);
+    alert(count)
+    count++;
+}*/
+
+
+/*var idade = prompt(" Qual sua idade");
+if(idade >= 18){
+    alert(" Maior de idade ");
+}else{
+    alert(" Menor de idade ");
+};*/
+
+
+
+
+//var frutas = [{nome: "maça", cor: "vermelha"},{nome: "uva", cor: "roxa"}]
+//console.log(frutas);
+//alert(frutas[1].nome);
+
+
+
+/*var fruta = {nome: "maça", cor: "vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);*/
+
+//var lista = ["maçã","pêra","laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista[0]);
+//console.log(lista.toString()[0]);
+//console.log(lista.join(' - '));
+
+
+
+
+//var nome = "Djailson Silva";
+//var n1 = 6;
+//var n2 = 10;
+//var frase = "Japão é o melhor time do mundo.";
+//alert(nome + " Tem " + idade + " anos ");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.toLowerCase());
+//alert(frase.replace("Japão","Brasil"))
+
